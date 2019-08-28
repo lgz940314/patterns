5ed32a73e154ca563f14555db136dc8d6669e934
liuguangzheng <rrefe4@163.com>
2019-08-28 10:13:36 +0800

    策略模式学习

---
+++ src/com/krupp/strategy/BlockEnemy.java
@@ -0,0 +1,16 @@
+package com.krupp.strategy;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 孙夫人断后，挡住追兵
+ */
+public class BlockEnemy implements IStrategy {
+
+    @Override
+    public void operate() {
+        System.out.println("孙夫人断后，挡住追兵");
+    }
+
+}
+
---
+++ src/com/krupp/strategy/GivenGreenLight.java
@@ -0,0 +1,14 @@
+package com.krupp.strategy;
+
+/**
+ * @author cbf4Life cbf4life@126.com
+ * I'm glad to share my knowledge with you all.
+ * 求吴国太开个绿灯
+ */
+public class GivenGreenLight implements IStrategy {
+    @Override
+    public void operate() {
+        System.out.println("求吴国太开个绿灯,放行！");
+    }
+
+}
